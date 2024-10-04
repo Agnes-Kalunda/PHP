54aa66073a40a1001ac7971f7d1b2a1950659ae5
Agnes <agypeter97@gmail.com>
2024-10-05 02:18:33 +0300

    add post form

--- pages/add_post.php
+++ pages/add_post.php
@@ -42,3 +42,28 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add new post</title>
+</head>
+<body>
+    <h2>Add new post</h2>
+
+    <form action="add_post.php" method="POST">
+    <label for="title">Title:</label>
+    <input type="text" name="title" required><br>
+    
+    <label for="content">Content:</label>
+    <textarea name="content" rows="5" required></textarea><br>
+    
+    <button type="submit">Add Post</button>
+</form>
+
+<a href="view_posts.php">View Posts</a>
+
+</body>
+</html>
