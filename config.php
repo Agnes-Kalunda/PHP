b84becdf157c254661b47e5f3922c18c03ddc306
Agnes <agypeter97@gmail.com>
2024-10-04 18:05:21 +0300

    db config

--- config.php
+++ config.php
@@ -1,11 +1,21 @@
 <?php
-$host ='';
-$dbname = '';
-$username ='';
-$password = '';
+
+
+require 'vendor/autoload.php';
+
+// Load .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$host = getenv('DB_HOST');
+$dbname   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSORD');
+
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
