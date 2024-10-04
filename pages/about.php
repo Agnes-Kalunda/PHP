99c854ab389c9074c672d1b21704b4766feb04aa
Agnes <agypeter97@gmail.com>
2024-10-04 17:30:01 +0300

    about us page

---
+++ pages/about.php
@@ -0,0 +1,44 @@
+<?php
+
+include '../config.php'
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+</head>
+<body>
+
+<header>
+    <h1>ABout Us</h1>
+
+    <nav>
+        <ul>
+        <li><a href="../index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+        <h2>Welcome to Our CMS</h2>
+        <p>
+            This is a basic Content Management System built with PHP and MySQL. 
+            Here, you can manage your content easily and efficiently.
+        </p>
+        <p>
+            Our goal is to provide a simple yet powerful platform for managing various types of content.
+        </p>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?>  All rights reserved.</p>
+    </footer>
+    
+</body>
+</html>
