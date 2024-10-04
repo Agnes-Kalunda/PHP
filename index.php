2efecab443ce5d0a35b83bca8ffeb5cd31251332
Agnes <agypeter97@gmail.com>
2024-10-04 17:23:39 +0300

    basic homepage

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include 'config.php'
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Hello!</h1>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="pages/about.php">About</a>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Latest Content</h2>
+        <p>Ths is where your content will appear</p>
+    </main>
+
+    <footer>
+    <p>&copy; 2024 My CMS. All rights reserved.</p>
+    </footer>
+    
+</body>
+</html>
