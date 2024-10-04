b0fc1c5c12b71ce499f421242eaa8fc54f6d8425
Agnes <agypeter97@gmail.com>
2024-10-05 00:49:28 +0300

    login form

--- pages/login.php
+++ pages/login.php
@@ -13,6 +13,18 @@ require '../config.php'
     <title>Login</title>
 </head>
 <body>
-    
+    <h2>Login</h2>
+
+    <form>
+        
+    </form><form action="login.php" method="POST">
+    <label for="username">Username:</label>
+    <input type="text" name="username" required><br>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" required><br>
+
+    <button type="submit">Login</button>
+</form>
 </body>
 </html>
