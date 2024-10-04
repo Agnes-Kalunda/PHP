8f048db1220fc95eaf700c06b0d73f956f8ec136
Agnes <agypeter97@gmail.com>
2024-10-04 17:13:01 +0300

    db config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$host ='';
+$dbname = '';
+$username ='';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
